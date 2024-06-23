37b1b60c4d79c6dc5faaeb167f40ab588cf1cc2c
San1things <businesslikesan1@gmail.com>
2024-06-23 20:28:48 +0800

    agaain

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -13,7 +13,7 @@
                 </a>
             </div>
         </div>
-        <div class="container p-4 mt-2 jlcard-container">
+        <div class="container p-4 mt-2 mb-5 jlcard-container">
             <div class="row row-cols-1 row-cols-lg-3 gy-3">
                 <div class="col">
                     <div class="card text-bg-light mb-3">
@@ -41,35 +41,38 @@
                 </div>
             </div>
         </div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    type="button" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item border">
-                            <a class="nav-link active" href="#" aria-current="page">View All</a>
-                        </li>
-                        <li class="nav-item border border-start-0">
-                            <a class="nav-link" href="#">Admins</a>
-                        </li>
-                        <li class="nav-item border border-start-0">
-                            <a class="nav-link" href="#">Staffs</a>
-                        </li>
-                        <li class="nav-item border border-start-0">
-                            <a class="nav-link" href="#">Others</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" aria-label="Search" placeholder="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+        <div class="users-table">
+            <h1>All users(000)</h1>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary p-3">
+                <div class="container-xl">
+                    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                        type="button" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item border">
+                                <a class="nav-link active" href="#" aria-current="page">View All</a>
+                            </li>
+                            <li class="nav-item border border-start-0">
+                                <a class="nav-link" href="#">Admins</a>
+                            </li>
+                            <li class="nav-item border border-start-0">
+                                <a class="nav-link" href="#">Staffs</a>
+                            </li>
+                            <li class="nav-item border border-start-0">
+                                <a class="nav-link" href="#">Others</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" aria-label="Search" placeholder="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </nav>
-
+            </nav>
+            
+        </div>
     </div>
 @endsection
